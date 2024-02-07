bb0f349fbe5ca98f3c3a7bd5fe52dc2c193f3de7
emre15900 <emresayinweb@gmail.com>
2024-02-07 15:19:12 +0300

    Structure created

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles/global.module.css";
+
+function HomePage() {
+  console.log(styles);
+  console.log("Hello Eteration!");
+
+  return (
+    <div style={{ width: "100%" }}>
+      <h1>Hello Eteration!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
