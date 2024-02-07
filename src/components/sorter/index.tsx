7e8e8bdd1ab59f90e2b51d047c3c0e1cd588885f
emre15900 <emresayinweb@gmail.com>
2024-02-07 19:05:04 +0300

    complete sort by component

--- src/components/sorter/index.tsx
+++ src/components/sorter/index.tsx
@@ -4,29 +4,118 @@ import {
   RadioGroup,
   FormControlLabel,
   FormControl,
-  FormLabel,
   Grid,
-  Card
+  Card,
+  Typography,
+  Divider,
 } from "@mui/material";
 
 function Sorter() {
+  const [selectedValue, setSelectedValue] = React.useState("oldToNew");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  console.log(selectedValue);
+
   return (
     <Grid>
-      <Card>
+      <Card
+        sx={{ background: "#000000", borderRadius: "20px", padding: "20px" }}
+      >
+        <Typography sx={{ color: "#ffffff", fontSize: 17 }}>Sort By</Typography>
+        <Divider sx={{ background: "#ffffff", mt: 1, mb: 1 }} />
         <FormControl>
-          <FormLabel id="sort-by">Sort By</FormLabel>
           <RadioGroup
             aria-labelledby="sort-by"
-            defaultValue="female"
-            name="radio-buttons-group"
+            defaultValue="oldToNew"
+            name="sort-by"
+            value={selectedValue}
+            onChange={handleChange}
           >
             <FormControlLabel
-              value="female"
-              control={<Radio />}
-              label="Female"
+              value="oldToNew"
+              control={
+                <Radio
+                  sx={{
+                    color: selectedValue === "oldToNew" ? "#66FF84" : "#ffffff",
+                    "&.Mui-checked": {
+                      color: "#66FF84",
+                    },
+                  }}
+                />
+              }
+              label="Old to new"
+              sx={{
+                color: selectedValue === "oldToNew" ? "#66FF84" : "#ffffff",
+                "&.Mui-checked": {
+                  color: "#66FF84",
+                },
+              }}
             />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <FormControlLabel
+              value="newToOld"
+              control={
+                <Radio
+                  sx={{
+                    color: selectedValue === "newToOld" ? "#66FF84" : "#ffffff",
+                    "&.Mui-checked": {
+                      color: "#66FF84",
+                    },
+                  }}
+                />
+              }
+              label="New to old"
+              sx={{
+                color: selectedValue === "newToOld" ? "#66FF84" : "#ffffff",
+                "&.Mui-checked": {
+                  color: "#66FF84",
+                },
+              }}
+            />
+            <FormControlLabel
+              value="hightToLow"
+              control={
+                <Radio
+                  sx={{
+                    color:
+                      selectedValue === "hightToLow" ? "#66FF84" : "#ffffff",
+                    "&.Mui-checked": {
+                      color: "#66FF84",
+                    },
+                  }}
+                />
+              }
+              label="Price high to low"
+              sx={{
+                color: selectedValue === "hightToLow" ? "#66FF84" : "#ffffff",
+                "&.Mui-checked": {
+                  color: "#66FF84",
+                },
+              }}
+            />
+            <FormControlLabel
+              value="lowToHight"
+              control={
+                <Radio
+                  sx={{
+                    color:
+                      selectedValue === "lowToHight" ? "#66FF84" : "#ffffff",
+                    "&.Mui-checked": {
+                      color: "#66FF84",
+                    },
+                  }}
+                />
+              }
+              label="Price low to high"
+              sx={{
+                color: selectedValue === "lowToHight" ? "#66FF84" : "#ffffff",
+                "&.Mui-checked": {
+                  color: "#66FF84",
+                },
+              }}
+            />
           </RadioGroup>
         </FormControl>
       </Card>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Header from "@/components/header";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Container } from "@mui/material";
 import Sorter from "@/components/sorter";
 
 function HomePage() {
   return (
     <div style={{ width: "100%" }}>
       <Header />
-      <Grid container spacing={6}>
-        <Grid item xs={12} sm={3} md={3}>
-          <Sorter />
+      <Container maxWidth="xl" sx={{ mt: 3}}>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={3} md={3}>
+            <Sorter />
+          </Grid>
+          <Grid item xs={12} sm={7} md={7}>
+            <Typography sx={{ color: "#000000" }}>Main</Typography>
+          </Grid>
+          <Grid item xs={12} sm={2} md={2}>
+            <Sorter />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={7} md={7}>
-          <Typography sx={{ color: "#000000" }}>Main</Typography>
-        </Grid>
-        <Grid item xs={12} sm={2} md={2}>
-          <Sorter />
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 }
