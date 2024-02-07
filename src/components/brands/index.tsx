e8c90a3f4bc8f33ca83d660bd1007a5dfcec397c
emre15900 <emresayinweb@gmail.com>
2024-02-07 20:07:08 +0300

    dynamic brand

--- src/components/brands/index.tsx
+++ src/components/brands/index.tsx
@@ -7,170 +7,45 @@ import {
   Card,
   Typography,
   Divider,
-  InputBase,
 } from "@mui/material";
 
-import { styled, alpha } from "@mui/material/styles";
-import SearchIcon from "@mui/icons-material/Search";
-
-function Brands() {
-  const [selectedValue, setSelectedValue] = React.useState(["apple"]);
-
-  const handleChange = (event: any) => {
-    const newValue = event.target.value;
-    setSelectedValue((prevSelectedValue) => {
-      if (prevSelectedValue.includes(newValue)) {
-        return prevSelectedValue.filter((value) => value !== newValue);
-      } else {
-        return [...prevSelectedValue, newValue];
-      }
-    });
-  };
-
-  console.log("Brands:", selectedValue);
-
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: "30px",
-    border: "1px solid #ffffff",
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "80%",
-    marginRight: 8,
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "#ffffff",
-    width: "100%",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      [theme.breakpoints.up("sm")]: {
-        width: "100%",
-        "&:focus": {
-          width: "100%",
-        },
-      },
-    },
-  }));
-
+function Brands({ brands, selectedBrands, onBrandChange }: any) {
   return (
     <Grid>
       <Card
         sx={{ background: "#000000", borderRadius: "20px", padding: "20px" }}
       >
-        <Grid
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            gap: 2,
-          }}
-        >
-          <Typography sx={{ color: "#ffffff", fontSize: 17 }}>
-            Brands
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon sx={{ color: "#ffffff" }} />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-        </Grid>
-
+        <Typography sx={{ color: "#ffffff", fontSize: 17 }}>Brands</Typography>
         <Divider sx={{ background: "#ffffff", mt: 1, mb: 1 }} />
-
         <FormGroup>
-          <FormControlLabel
-            value="apple"
-            control={
-              <Checkbox
-                checked={selectedValue.includes("apple")}
-                sx={{
-                  color: selectedValue.includes("apple")
-                    ? "#66FF84"
-                    : "#ffffff",
-                  "&.Mui-checked": {
-                    color: selectedValue.includes("apple")
+          {brands.map((brand: any) => (
+            <FormControlLabel
+              key={brand.value}
+              value={brand.value}
+              control={
+                <Checkbox
+                  checked={selectedBrands.includes(brand.value)}
+                  onChange={onBrandChange}
+                  sx={{
+                    color: selectedBrands.includes(brand.value)
                       ? "#66FF84"
                       : "#ffffff",
-                  },
-                }}
-                onChange={handleChange}
-              />
-            }
-            label="Apple"
-            sx={{
-              color: selectedValue.includes("apple") ? "#66FF84" : "#ffffff",
-            }}
-          />
-          <FormControlLabel
-            value="samsung"
-            control={
-              <Checkbox
-                checked={selectedValue.includes("samsung")}
-                sx={{
-                  color: selectedValue.includes("samsung")
-                    ? "#66FF84"
-                    : "#ffffff",
-                  "&.Mui-checked": {
-                    color: selectedValue.includes("samsung")
-                      ? "#66FF84"
-                      : "#ffffff",
-                  },
-                }}
-                onChange={handleChange}
-              />
-            }
-            label="Samsung"
-            sx={{
-              color: selectedValue.includes("samsung") ? "#66FF84" : "#ffffff",
-            }}
-          />
-          <FormControlLabel
-            value="huawei"
-            control={
-              <Checkbox
-                checked={selectedValue.includes("huawei")}
-                sx={{
-                  color: selectedValue.includes("huawei")
-                    ? "#66FF84"
-                    : "#ffffff",
-                  "&.Mui-checked": {
-                    color: selectedValue.includes("huawei")
-                      ? "#66FF84"
-                      : "#ffffff",
-                  },
-                }}
-                onChange={handleChange}
-              />
-            }
-            label="Huawei"
-            sx={{
-              color: selectedValue.includes("huawei") ? "#66FF84" : "#ffffff",
-            }}
-          />
+                    "&.Mui-checked": {
+                      color: selectedBrands.includes(brand.value)
+                        ? "#66FF84"
+                        : "#ffffff",
+                    },
+                  }}
+                />
+              }
+              label={brand.label}
+              sx={{
+                color: selectedBrands.includes(brand.value)
+                  ? "#66FF84"
+                  : "#ffffff",
+              }}
+            />
+          ))}
         </FormGroup>
       </Card>
     </Grid>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "@/components/header";
 import { Grid, Typography, Container } from "@mui/material";
 import Sorter from "@/components/sorter";
 import Brands from "@/components/brands";
 
 function HomePage() {
+  const [selectedBrands, setSelectedBrands] = useState(["apple"]);
+
+  const handleBrandChange = (event: any) => {
+    const brand = event.target.value;
+    setSelectedBrands((prevSelectedBrands) => {
+      if (prevSelectedBrands.includes(brand)) {
+        return prevSelectedBrands.filter(
+          (selectedBrand) => selectedBrand !== brand
+        );
+      } else {
+        return [...prevSelectedBrands, brand];
+      }
+    });
+  };
+
+  console.log("Brands:", selectedBrands);
+
+  const brands = [
+    { label: "Apple", value: "apple" },
+    { label: "Samsung", value: "samsung" },
+    { label: "Huawei", value: "huawei" },
+  ];
+
   return (
     <div style={{ width: "100%" }}>
       <Header />
@@ -15,8 +38,12 @@ function HomePage() {
               <Grid>
                 <Sorter />
               </Grid>
-              <Grid sx={{ mt: 2}}>
-                <Brands />
+              <Grid sx={{ mt: 2 }}>
+                <Brands
+                  brands={brands}
+                  selectedBrands={selectedBrands}
+                  onBrandChange={handleBrandChange}
+                />
               </Grid>
             </Grid>
           </Grid>
