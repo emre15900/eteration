e97537ee861a0e57083b20c3a190a35187e2be96
emre15900 <emresayinweb@gmail.com>
2024-02-09 01:36:59 +0300

    favorite page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectFavorites } from "@/store/apps/favoritesSlice";
 import { RootState } from "@/store/store";
+import ProductCard from "@/components/productCard";
 
 const Favorites = () => {
-  const favorites = useSelector((state: RootState) => selectFavorites(state));
-  console.log("favorites:", favorites);
+  const favoriteIds = useSelector((state: RootState) => selectFavorites(state));
+  const products = useSelector((state: RootState) => state.products.products);
+
+  const favoriteProducts = products.filter((product) =>
+    favoriteIds.includes(product.id)
+  );
+
   return (
     <div>
-      <h2>Favori Ürünlerim</h2>
-      <ul>
-        {favorites.map((favoriteId) => (
-          <li key={favoriteId}>{/* Favori ürünü burada göster */}</li>
+      <h1>Favori Ürünlerim</h1>
+      <div>
+        {favoriteProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
